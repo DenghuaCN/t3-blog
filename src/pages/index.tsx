8fa8dd7db9ed23aff5768ece3d6b2ff3c15c4523
denghua <denghua.fm@foxmail.com>
2024-01-02 15:53:41 +0800

    finish sidebar html part of 1

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,7 @@ import { HiChevronDown } from "react-icons/hi";
 
 const HomePage = () => {
   return (
-    <div className="flex h-screen w-full flex-col">
+    <div className="flex h-full w-full flex-col">
       {/* Header */}
       <header className="flex h-20 w-full flex-row items-center justify-around border-b-[1px] border-gray-300 bg-white">
         {/* Left More */}
@@ -53,7 +53,7 @@ const HomePage = () => {
         </div>
       </header>
 
-      {/* Main Screen */}
+      {/* 左栏 Main Screen */}
       <section className="grid h-full w-full grid-cols-12 place-items-center">
         {/* 主内容 */}
         <main className="col-span-8 h-full w-full border-r border-gray-300 px-24">
@@ -130,12 +130,17 @@ const HomePage = () => {
           {/* 博文内容 ExcelRow */}
           <div className="flex w-full flex-col justify-center space-y-8">
             {Array.from({ length: 5 }).map((_, i) => (
-              <div key={i} className="flex flex-col group space-y-4 border-b border-gray-300 pb-8 last:border-none">
+              <div
+                key={i}
+                className="group flex flex-col space-y-4 border-b border-gray-300 pb-8 last:border-none"
+              >
                 {/* 博文简介 */}
                 <div className="flex w-full items-center space-x-2">
-                  <div className="w-10 h-10 bg-gray-400 rounded-full"></div>
+                  <div className="h-10 w-10 rounded-full bg-gray-400"></div>
                   <div>
-                    <p className="font-semibold">John Soni &#x2022; 22 Dec. 2022</p>
+                    <p className="font-semibold">
+                      John Soni &#x2022; 22 Dec. 2022
+                    </p>
                     <p className="text-sm">Founder, teacher & developer</p>
                   </div>
                 </div>
@@ -144,25 +149,18 @@ const HomePage = () => {
                 <div className="grid w-full grid-cols-12 gap-4">
                   <div className="col-span-8 flex flex-col space-y-4">
                     {/* 标题 */}
-                    <p className="text-2xl font-bold text-gray-800 group-hover:underline decoration-gray-800">
+                    <p className="text-2xl font-bold text-gray-800 decoration-gray-800 group-hover:underline">
                       Lorem ipsum dolor sit amet, consectetur adipisicing elit.
                       Nisi, quaerat.
                     </p>
                     {/* 主内容 */}
-                    <p className="text-sm text-gray-500 break-words">
-                      Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                      A nostrum possimus, rem unde incidunt voluptates
-                      praesentium sapiente aut recusandae dolore totam nulla
-                      officiis, sit, ipsum neque similique autem dolorem ex
-                      expedita quam? Perspiciatis provident minima delectus
-                      animi, veniam et mollitia rerum corporis doloribus qui.
-                      Expedita, provident velit maiores modi dolor ipsa nisi
-                      placeat libero pariatur rerum vel eveniet voluptas amet?
+                    <p className="break-words text-sm text-gray-500">
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia molestias at adipisci dolore necessitatibus amet sapiente, consequuntur iure cum, molestiae explicabo cupiditate corrupti beatae vel, corporis harum dolor laborum ab tempora quis aut accusamus voluptatem porro! Nemo incidunt ipsa ea error unde, ullam, officia reprehenderit quisquam iste id veniam aspernatur iusto libero, velit quos alias quae corrupti quod? Odit, excepturi.
                     </p>
                   </div>
                   {/* 标题图 */}
                   <div className="col-span-4">
-                    <div className="h-full w-full rounded-xl bg-gray-300 transition hover:scale-105 transform duration-300 hover:shadow-xl"></div>
+                    <div className="h-full w-full transform rounded-xl bg-gray-300 transition duration-300 hover:scale-105 hover:shadow-xl"></div>
                   </div>
                 </div>
 
@@ -187,8 +185,73 @@ const HomePage = () => {
         </main>
 
         {/* 右栏 */}
-        <aside className="col-span-4 h-full w-full">
-          this is used for side bar
+        <aside className="sticky top-20 col-span-4 flex h-full w-full flex-col space-y-4 p-6">
+          {/* 可能感兴趣的用户 */}
+          <div>
+            <h3 className="my-6 text-lg font-semibold">
+              People you might be interested
+            </h3>
+            <div className="flex flex-col space-y-4">
+              {Array.from({ length: 4 }).map((_, i) => (
+                <div key={i} className="flex flex-row items-center space-x-5">
+                  <div className="h-10 w-10 flex-none rounded-full bg-gray-300"></div>
+
+                  <div>
+                    <div className="text-sm font-bold text-gray-900">
+                      John Doe
+                    </div>
+                    <div className="text-xs">
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Repellat, itaque ea. Tenetur, dolore?
+                    </div>
+                  </div>
+
+                  <div>
+                    <button
+                      className="
+                        flex
+                        items-center
+                        space-x-3
+                        rounded
+                        border
+                        border-gray-400/50
+                        px-4
+                        py-2
+                        transition
+                        hover:border-gray-900
+                        hover:text-gray-900"
+                    >
+                      Follow
+                    </button>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+          {/* 阅读列表 */}
+          <div>
+            <h3 className="my-6 text-lg font-semibold">Your reading list</h3>
+            <div className="flex flex-col space-y-8">
+              {Array.from({ length: 4 }).map((_, i) => (
+                <div key={i} className="group flex items-center space-x-6">
+                  <div className="aspect-square h-full w-2/5 rounded-xl bg-gray-300"></div>
+                  <div className="flex w-3/5 flex-col space-y-2">
+                    <div className="text-lg font-semibold decoration-gray-800 group-hover:underline">
+                      Lorem ipsum dolor sit amet consectetur.
+                    </div>
+                    <div className="break-words">
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, aut.
+                    </div>
+                    <div className="flex w-full items-center space-x-4">
+                      <div className="h-8 w-8 rounded-full bg-gray-300"></div>
+                      <div className="">John Doe &#x2022;</div>
+                      <div>Dec 22, 2022</div>
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
         </aside>
       </section>
     </div>
